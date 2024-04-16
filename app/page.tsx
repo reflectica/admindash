0ffa4eab6b6b64e9c5e4a2fc882de2da6d8c5be6
Chris Hong <hongchris97@gmail.com>
2024-04-15 23:37:24 -0700

    button to take to dashboard (temporary)"

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const route = useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+     <h1>LANDING PAGE</h1>
+     <button className='border-solid border-2 p-2 rounded-md hover:bg-slate-200' onClick={() => route.push('/dashboard')}>Go To Dash</button>
     </main>
   );
 }
