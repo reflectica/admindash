c2f4f682291fa7a73146949b10bcbc851e268da2
Chris Hong <hongchris97@gmail.com>
2024-04-16 23:50:14 -0700

    landing

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,13 @@ export default function Home() {
   const route = useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>LANDING PAGE</h1>
-     <button className='border-solid border-2 p-2 rounded-md hover:bg-slate-200' onClick={() => route.push('/dashboard')}>Go To Dash</button>
+      <h1>LANDING PAGE</h1>
+      <button 
+        className='border-solid border-2 p-2 rounded-md hover:bg-slate-200' 
+        onClick={() => route.push('/dashboard')}
+      >
+        Go To Dash
+      </button>
     </main>
   );
 }
