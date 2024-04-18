5dd7b7631ff06c4deb831f330375f7866815fe1b
Chris Hong <hongchris97@gmail.com>
2024-04-17 21:28:21 -0700

    barchart

--- components/graph/BarChart.tsx
+++ components/graph/BarChart.tsx
@@ -5,30 +5,30 @@ import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Toolti
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export default function BarChart() {
-    const data = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'Monthly Sales',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-    };
-    
-    const options = {
-        scales: {
-            y: {
-            beginAtZero: true,
-            },
+  const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Monthly Sales',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+          ],
+          borderWidth: 1,
         },
-    };
+      ],
+  };
+  
+  const options = {
+    scales: {
+        y: {
+        beginAtZero: true,
+        },
+    },
+  };
 
-    return <Bar data={data} options={options} />;
+  return <Bar data={data} options={options} />;
 }
